73b691adfd2a4e351adcf8658ef37b04bf30b10b
ManchiGithub <btlltk13@gmail.com>
2024-02-23 00:06:20 +0200

    Enable edge to edge

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import theme.MyApplicationTheme
 
@@ -11,6 +13,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             MyApplicationTheme {
                 Surface {
